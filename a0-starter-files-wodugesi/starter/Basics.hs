e578adb6391b86d91cf2c834abb12082c6793b41
wodugesi <wodugesi@teach.cs.utoronto.ca>
2025-09-07 17:50:16 -0400

    Finished the assignment

--- a0-starter-files-wodugesi/starter/Basics.hs
+++ a0-starter-files-wodugesi/starter/Basics.hs
@@ -2,10 +2,16 @@ module Basics where
 
 {-------------------- Basic FP --------------------}
 power5 :: Int -> Int
-power5 n = error "TODO"
+power5 n = n ^ 5
 
 ageDiscount :: Int -> Float -> Float
-ageDiscount age basePrice = error "TODO"
+ageDiscount age basePrice
+    | age < 18 = basePrice * 0.8
+    | age >= 60 = basePrice * 0.8
+    | otherwise = basePrice * 1
 
 squareTheDiff :: Int -> Int -> Int
-squareTheDiff x y = error "TODO"
+squareTheDiff x y =
+    let diff = x - y
+    in diff ^ 2
+
